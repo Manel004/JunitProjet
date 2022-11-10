4d8768e14bfa58e1a86e5bb7e1748e1a4ab30200
Manel <manel.zekri004@gmail.com>
2022-11-10 06:48:52 +0100

    Create Junit Test Project

---
+++ src/main/java/JUnitClasses/Calculator.java
@@ -0,0 +1,15 @@
+package JUnitClasses;
+
+public class Calculator {
+
+    public int add(int firstNumber,int secondNumber)
+    {
+        int total=firstNumber+secondNumber;
+
+        return total;
+
+    }
+
+
+
+}
